04d5e25b52033baf58c02809d15f9805550b4e07
mohamed <berkaouimed1@gmail.com>
2025-03-28 00:59:03 +0100

    first

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,13 +7,13 @@ import { useEffect } from "react";
 export default function Home() {
   const { user } = useAuth();
   const router = useRouter();
-
   useEffect(() => {
-    if (user) {
-      router.push("/dashboard");
-    } else {
-      router.push("/user/login");
-    }
+    router.push("/dashboard");
+    // if (user) {
+    //   router.push("/dashboard");
+    // } else {
+    //   router.push("/user/login");
+    // }
   }, [user, router]);
 
   return null;
